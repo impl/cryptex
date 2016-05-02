e9a9d595693b3fb436359931c6914dceef13c966
Noah Fontes <nfontes@cynigram.com>
2016-05-02 10:47:51 -0700

    Add code coverage

--- mix.exs
+++ mix.exs
@@ -2,19 +2,30 @@ defmodule ExCrypto.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ex_crypto,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :ex_crypto,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        'coveralls.travis': :test,
+      ]
+    ]
   end
 
   def application do
-    [applications: [:logger]]
+    [
+      applications: [:logger]
+    ]
   end
 
   defp deps do
-    []
+    [
+      {:excoveralls, "~> 0.5.3", only: :test},
+    ]
   end
 end
